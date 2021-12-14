58b258f228f479e669659a73c40613fca92493c4
Syed Andleeb <andleebsyed75@gmail.com>
2021-12-14 16:46:23 +0530

    update: added Slogan instruction process component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
+import { Process } from "./components/Process/Process";
 import { Slogan } from "./components/Slogan/Slogan";
 function App() {
   return (
     <div className="App">
       <Header />
       <Slogan />
+      <Process />
     </div>
   );
 }
